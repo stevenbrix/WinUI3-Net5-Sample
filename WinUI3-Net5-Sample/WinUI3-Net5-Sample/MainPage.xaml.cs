5f0fd31496306134ced5afd5b8da8c823f7706a9
Steve Kirbach <steven.kirbach@gmail.com>
2020-11-18 13:04:40 -0800

    Add project files.

---
+++ WinUI3-Net5-Sample/WinUI3-Net5-Sample/MainPage.xaml.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.ViewManagement;
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace WinUI3_Net5_Sample
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            appType = InAppContainer ? "UWP" : "Win32";
+        }
+
+        public bool InAppContainer => (App.Current as App).InAppContainer;
+
+        private string appType;
+        public string AppType
+        {
+            get => $"Hello from {appType}";
+        }
+
+        async private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            ApplicationView currentView = ApplicationView.GetForCurrentView();
+            ApplicationViewMode currentMode = currentView.ViewMode;
+            ApplicationViewMode nextMode = currentMode == ApplicationViewMode.Default ? ApplicationViewMode.CompactOverlay : ApplicationViewMode.Default;
+            if (currentView.IsViewModeSupported(nextMode))
+            {
+                await currentView.TryEnterViewModeAsync(nextMode);
+            }
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,24 @@
+# WinUI3 .NET5 Sample
+
+This repository contains a sample WinUI3 app that builds using .NET5. With a simple change of one project property in the .csproj,
+the application can either be launched in a UWP or Win32 context.
+
+In the WinUI3-NET5-Sample.csproj, is the following:
+
+```xml
+    <!-- Set this to 'false' if you want your app to launch as a Win32 application -->
+    <WindowsAppContainer>true</WindowsAppContainer>
+```
+
+You can use this property to toggle between the application running as a packaged UWP application,
+or a packaged Win32 application.
+
+| WindowsAppContainer| App type |
+|        :-:         |    :-:   |
+|  `true`            | UWP      |
+|  `false`           | Win32    |
+|   Not defined      | Win32    |
+
+## Known Issues
+
+When you switch the value of `WindowsAppContainer` you need to do a full rebuild.
